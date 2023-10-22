9356f73763c5e0590c5d59898b6b3e60e169ae2d
Eve-Dima <eve.dima@mail.ru>
2023-10-22 22:29:57 +0300

    Init commit

---
+++ src/test/java/com/practice/department_manager/DepartmentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.department_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
